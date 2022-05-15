b0376ac4ec0f918cf8da29d548abf9943d4c26ab
julianferres <julianferres@gmail.com>
2022-05-15 17:29:58 -0300

    Uso polybar

--- refresh_dotfiles.sh
+++ refresh_dotfiles.sh
@@ -28,6 +28,7 @@ copy_if_exists ~/.config/qtile ~/dotfiles/config
 copy_if_exists ~/.config/rofi/config.rasi ~/dotfiles/config
 copy_if_exists ~/.config/i3 ~/dotfiles/config
 copy_if_exists ~/.config/i3status ~/dotfiles/config
+copy_if_exists ~/.config/polybar ~/dotfiles/config
 
 # powerlevel10k config
 cp ~/.p10k.zsh ~/dotfiles/rc
